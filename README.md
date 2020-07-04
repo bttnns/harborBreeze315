c6642a9058afddf69809a5d7f7fc7102e3829973
Brian Tannous <brian@briantannous.com>
2020-07-04 18:20:24 -0500

    Adding codes and docs

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Controll your Harbor Breeze ceiling fan with a Raspberry Pi and a FS1000A (315Mh
 
 Writing this to have a simple API for my Harbor Breeze Ceiling fan to be controlled by [Home Assistant](https://hass.io)
 
-The Remote has a [FCC ID = A25-TX012](fcc.io/A25-TX012), DIP Switch under the battery = 1
+The Remote has a [FCC ID = A25-TX012](https://fcc.io/A25-TX012), DIP Switch under the battery = 1
 I'm using the commonly found for cheap [FS1000A Transmitter](https://www.amazon.com/HiLetgo-Transmitter-Receiver-Arduino-Raspberry/dp/B00LNADJS6)
 
 # Usage
@@ -14,4 +14,12 @@ FS1000A:Pi GPIO
 - VCC:5V
 - DATA: GPIO 23
 
+Set the configurables at the top of `app.py`.
 
+Then run the script to TX the code where `function = variable name of command`
+`python3 app.py <function>`
+
+Examples:
+- `python3 app.py fs4` set Fan Speed 4, Summer Direction
+- `python3 app.py liT` toggle Light ON/OFF
+ 
--- app.py
+++ app.py
@@ -2,18 +2,59 @@ import time
 import sys
 import RPi.GPIO as GPIO
 
-f1 = '0011000011111110101110010'
-f2 = '0011000011111110110110010'
-f3 = '0011000011111110100110010'
-f4 = ''
-f5 = ''
-f6 = ''
+# Configurables
+NUM_ATTEMPTS = 6 # How many times to repeat the code
+TRANSMIT_PIN = 23 # GPIO Pin for FS1000A Data
+DIP = 1 # Remote DIP Switch: 0 or 1
 
+# Delays for keying, should not need to change these
 short_delay = 0.00038
 long_delay = 0.00077
 extended_delay = 0.008
-NUM_ATTEMPTS = 4
-TRANSMIT_PIN = 23
+
+# Harbor breeze remote bits See the manual for the button # mapping
+# https://fccid.io/A25-TX012/User-Manual/User-manual-1937614
+
+# Preambles depending on remote DIP position. Code = dpX + func
+dp0 = '00000000000000000' # Dip Switch 0 Preamble
+dp1 = '00110000111111101' # Dip Switch 1 Preamble
+
+# Fan button functions. Button #1 in user manual
+# Winter (Counterclockwise)
+fw1 = '01110010' # Fan Speed 1
+fw2 = '10110010' # Fan Speed 2
+fw3 = '00110010' # Fan Speed 3
+fw4 = '11010010' # Fan Speed 4
+fw5 = '01010010' # Fan Speed 5
+fw6 = '10010010' # Fan Speed 6
+fwT = '11110010' #2 Fan ON/OFF
+fwN = '00010010' #3 Nature Breeze
+
+# Summer (Clockwise)
+fs1 = '01111010' # Fan Speed 1
+fs2 = '10111010' # Fan Speed 2
+fs3 = '00111010' # Fan Speed 3
+fs4 = '11011010' # Fan Speed 4
+fs5 = '01011010' # Fan Speed 5
+fs6 = '10011010' # Fan Speed 6
+fsT = '11111010' #2 Fan ON/OFF
+fsN = '00011010' #3 Nature Breeze
+
+# Fan Direction
+fwD = '11100010' #8 Fan Winter Direction
+fsD = '11101010' #8 Fan Summer Direction
+
+# Light button mappings
+liH = '00001110' #4 Home Shield (Lights cycle on for 5-20 minutes and off for 60 minutes, simulating occupancy)
+liT = '01101010' #5 Light ON/OFF
+liD = '10101010' #5 Light DIMMING
+
+# Delay button mappings
+deO = '00100010' #6 Delay Off
+de2 = '01101110' #7 Delay 2H
+de4 = '10101110' #7 Delay 4H
+de8 = '00101110' #8 Delay 8H
+
 
 def transmit_code(code):
     '''Transmit a chosen code string using the GPIO transmitter'''
@@ -39,5 +80,7 @@ def transmit_code(code):
 
 if __name__ == '__main__':
     for argument in sys.argv[1:]:
-        exec('transmit_code(' + str(argument) + ')')
-
+        if DIP == 0:
+            exec('transmit_code(dp0 + ' + str(argument) + ')')
+        elif DIP == 1:
+            exec('transmit_code(dp1 + ' + str(argument) + ')')
